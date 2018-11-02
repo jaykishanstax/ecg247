4fd9f07b9b6ccdbd9fa8d64a9015146631e34dc9
Jaykishan Hirpara <jaykishan@supertax.in>
2018-11-02 12:12:34 +0530

    Added user controler working

--- ecgonline/src/main/java/com/ecg/web/ecgonline/controller/UserController.java
+++ ecgonline/src/main/java/com/ecg/web/ecgonline/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.ecg.web.ecgonline.controller;
 
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
 
+	 @RequestMapping(value = "ping", method = RequestMethod.GET)
+	    public ResponseEntity ping(final HttpServletRequest request) {
+	        return ResponseEntity.ok("pong");
+	    }
+	
 }
