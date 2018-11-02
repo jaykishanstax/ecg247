4645b0ed5c27363a4ba450a7e087e6784dbb2ee0
Jaykishan Hirpara <jaykishan@supertax.in>
2018-11-02 11:46:28 +0530

    Dev initial upload file

---
+++ ecgonline/src/main/java/com/ecg/web/ecgonline/EcgonlineApplication.java
@@ -0,0 +1,12 @@
+package com.ecg.web.ecgonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcgonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcgonlineApplication.class, args);
+	}
+}
